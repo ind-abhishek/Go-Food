0332e3e25899fc0b9370d8d0d49ef1f55adbab29
Abhishek Sharma <abhishek859573@gmail.com>
2024-04-11 18:22:45 +0530

    new dfiles

--- food-app/backend/index.js
+++ food-app/backend/index.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000;
 const mongoDb = require("./db");
 mongoDb();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow requests from this origin
+  })
+);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
 
 app.use(express.json());
--- food-app/src/App.jsx
+++ food-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./screen/Home";
 import Login from "./screen/Login";
+import SignUp from "./screen/SignUp";
 import Carousel from "react-bootstrap/Carousel";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/createuser" element={<SignUp />} />
         </Routes>
       </div>
     </BrowserRouter>
